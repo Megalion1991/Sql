8d410c533c685b3e7a4867078a3d91bfb8fa9d34
Megalion1991 <96067574+Megalion1991@users.noreply.github.com>
2023-01-28 08:18:19 -0800

    Add files via upload

---
+++ Database.sql
@@ -0,0 +1,17 @@
+---show the list of database
+SELECT name, database_id, create_date  
+FROM sys.databases ;  
+
+---drop database
+
+DROP DATABASE my_test
+DROP DATABASE tele_sales
+
+---Create a database
+
+CREATE DATABASE customer
+
+----USE A DATABASE
+
+USE customer
+
