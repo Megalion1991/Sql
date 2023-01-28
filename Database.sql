8fee9be188b46446ad20d2a244a967e55b73e40b
Adekoya AbdulRasaq Eniola <96067574+Megalion1991@users.noreply.github.com>
2023-01-28 08:43:12 -0800

    Update Database.sql

--- Database.sql
+++ Database.sql
@@ -1,3 +1,7 @@
+---Database is an organised collection of datastored and assessed automatically,Relational Database contains Entity which is an object we stored something about e.g person
+---Attribute: are the characteristics of an object e.g name ,Variables: are the data store in the column...customer(entity) name(attribute) tola(variable)
+
+
 ---show the list of database
 SELECT name, database_id, create_date  
 FROM sys.databases ;  
